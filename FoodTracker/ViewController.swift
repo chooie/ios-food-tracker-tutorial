805d92397c5e359d4efff6a1c34b13439d3d0f2b
Charlie Hebert <chebert@spotxchange.com>
2016-12-22 11:34:50 -0700

    Initial commit with basic view stubbed out

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Charlie Hebert on 12/20/16.
+//  Copyright © 2016 Charlie Hebert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+}
+
