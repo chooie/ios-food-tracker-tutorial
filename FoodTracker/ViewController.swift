2dcf58332e9b7905a3bfd72cc299d66a30ebd9c8
Charlie Hebert <chebert@spotxchange.com>
2016-12-22 16:04:41 -0700

    Add outlets and actions to ViewController from the Main storyboard

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,11 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+
+  //MARK: Properties
+  @IBOutlet weak var mealNameLabel: UILabel!
+  @IBOutlet weak var nameTextField: UITextField!
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    nameTextField.delegate = self
+  }
+
+  //MARK: UITextFieldDelegate
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    textField.resignFirstResponder()
+    return true
+  }
+
+  func textFieldDidEndEditing(_ textField: UITextField) {
+    mealNameLabel.text = nameTextField.text
+  }
+
+  //MARK: Actions
+  @IBAction func setDefaultLabelText(_ sender: UIButton) {
+    mealNameLabel.text = "Default Text"
   }
 }
 
