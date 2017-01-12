0985e672b0c235358255340712e13873fb57ecb5
Charlie Hebert <chebert@spotxchange.com>
2017-01-12 14:26:18 -0700

    Add Meal model. Add unit tests for Meal model

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,31 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Charlie Hebert on 1/12/17.
+//  Copyright © 2017 Charlie Hebert. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+
+  //MARK: Properties
+  var name: String
+  var photo: UIImage?
+  var rating: Int
+
+  //MARK: Initialization
+  init?(name: String, photo: UIImage?, rating: Int) {
+    guard !name.isEmpty else {
+      return nil
+    }
+    guard (rating >= 0) && (rating <= 5) else {
+      return nil
+    }
+
+    self.name = name
+    self.photo = photo
+    self.rating = rating
+  }
+}
